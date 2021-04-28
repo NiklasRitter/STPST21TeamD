5fd62ff7d5712b9156a2082068efe6054410a955
Eric <ericrode13@gmail.com>
2021-04-28 21:23:50 +0200

    feat: load view references for login button

--- src/main/java/de/uniks/stp/wedoit/accord/client/controller/LoginScreenController.java
+++ src/main/java/de/uniks/stp/wedoit/accord/client/controller/LoginScreenController.java
@@ -2,14 +2,20 @@ package de.uniks.stp.wedoit.accord.client.controller;
 
 import de.uniks.stp.wedoit.accord.client.Editor;
 import de.uniks.stp.wedoit.accord.client.model.LocalUser;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class LoginScreenController {
 
     private LocalUser localUser;
     private Editor editor;
     private Parent view;
+    private Button btnLogin;
+    private TextField tfUserName;
+    private TextField pwUserPw;
 
     public LoginScreenController(Parent view, LocalUser model, Editor editor) {
         this.view = view;
@@ -18,6 +24,17 @@ public class LoginScreenController {
     }
 
     public void init() {
+        //Load all view references
+        tfUserName = (TextField) view.lookup("#tfUserName");
+        pwUserPw = (TextField) view.lookup("#pwUserPw");
+
+        btnLogin = (Button) view.lookup("#btnLogin");
+
+        btnLogin.setOnAction(this::loginButtonAction);
+
+    }
+
+    private void loginButtonAction(ActionEvent actionEvent) {
     }
 
     public void stop() {
