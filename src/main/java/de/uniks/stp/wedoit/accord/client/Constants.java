8879fd378715c969d1a7a5fb90df51671965a724
Eric <ericrode13@gmail.com>
2021-04-29 17:22:41 +0200

    feat: add login function to RestClient
    Add login function to RestClient that builds JSON message with name and password of user

--- src/main/java/de/uniks/stp/wedoit/accord/client/Constants.java
+++ src/main/java/de/uniks/stp/wedoit/accord/client/Constants.java
@@ -1,4 +1,13 @@
 package de.uniks.stp.wedoit.accord.client;
 
 public class Constants {
+
+    public static final String USERS_PATH = "/users";
+    public static final String LOGIN_PATH = "/login";
+
+    public static final String REST_SERVER_URL = "https://ac.uniks.de/api";
+
+    //Json fields
+    public static final String COM_NAME = "name";
+    public static final String COM_PASSWORD = "password";
 }
--- src/main/java/de/uniks/stp/wedoit/accord/client/network/RestClient.java
+++ src/main/java/de/uniks/stp/wedoit/accord/client/network/RestClient.java
@@ -1,4 +1,25 @@
 package de.uniks.stp.wedoit.accord.client.network;
 
+import javax.json.Json;
+import kong.unirest.*;
+
+import static de.uniks.stp.wedoit.accord.client.Constants.*;
+
 public class RestClient {
+
+    public static void login(String name, String password, Callback<JsonNode> callback) {
+        // Build Request Body
+        String body = Json.createObjectBuilder().add(COM_NAME, name).add(COM_PASSWORD, password).build().toString();
+
+        // Use UniRest to make login request
+        HttpRequest<?> req = Unirest.post( REST_SERVER_URL + USERS_PATH + LOGIN_PATH)
+                .header(COM_NAME, name)
+                .body(body);
+
+        sendRequest(req, callback);
+    }
+
+    private static void sendRequest(HttpRequest<?> req, Callback<JsonNode> callback) {
+        new Thread(() -> req.asJsonAsync(callback)).start();
+    }
 }
