80d146e8715b360ae8f5b54da6f204b51a4ff920
Alex <alexander-rode1@web.de>
2021-04-30 21:37:02 +0200

    feat: add haveServer method and add class for MainScreen listView
    haveServer: method to create new server instances;
    class added that only the name of a server is shown in the listView of the MainScreen

--- src/main/java/de/uniks/stp/wedoit/accord/client/Editor.java
+++ src/main/java/de/uniks/stp/wedoit/accord/client/Editor.java
@@ -1,10 +1,33 @@
 package de.uniks.stp.wedoit.accord.client;
 
 import de.uniks.stp.wedoit.accord.client.model.LocalUser;
+import de.uniks.stp.wedoit.accord.client.model.Server;
 
 public class Editor {
 
     public LocalUser haveLocalUser() {
         return new LocalUser();
     }
+
+    /**
+     * create a sever with the given arguments and with localUser as Member
+     *
+     * @param localUser localUser is member of the server with following id and name
+     * @param id        id of the sever
+     * @param name      name of the server
+     * @return server with given id and name and with member localUser
+     */
+    public Server haveServer(LocalUser localUser, String id, String name) {
+        if (localUser.getServers() != null) {
+            for (Server server : localUser.getServers()) {
+                if (server.getId() == id) {
+                    return server;
+                }
+            }
+        }
+        Server server = new Server().setLocalUser(localUser).setId(id).setName(name);
+        return server;
+    }
+
+
 }
---
+++ src/main/java/de/uniks/stp/wedoit/accord/client/view/MainScreenServerListView.java
@@ -0,0 +1,25 @@
+package de.uniks.stp.wedoit.accord.client.view;
+
+
+import de.uniks.stp.wedoit.accord.client.model.Server;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+
+public class MainScreenServerListView implements javafx.util.Callback<ListView<Server>, ListCell<Server>> {
+
+    @Override
+    public ListCell<Server> call(ListView<Server> param) {
+        return new ServerListCell();
+    }
+
+
+    private static class ServerListCell extends ListCell<Server> {
+        protected void updateItem(Server item, boolean empty) {
+            super.updateItem(item, empty);
+            if (!empty) {
+                this.setText(item.getName());
+            }
+        }
+    }
+
+}
