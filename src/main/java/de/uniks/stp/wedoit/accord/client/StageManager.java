be38a94a777e2bfc9aebede1c0caf469b9cea93a
Alex <alexander-rode1@web.de>
2021-05-02 19:11:49 +0200

    feat: add implementation of showOption and showWelcome in Stagemanager
    MainScreenController::welcomeButtonOnClick and
    MainScreenController::settingsButtonOnClick need the added functions to work correctly

--- src/main/java/de/uniks/stp/wedoit/accord/client/StageManager.java
+++ src/main/java/de/uniks/stp/wedoit/accord/client/StageManager.java
@@ -2,6 +2,8 @@ package de.uniks.stp.wedoit.accord.client;
 
 import de.uniks.stp.wedoit.accord.client.controller.LoginScreenController;
 import de.uniks.stp.wedoit.accord.client.controller.MainScreenController;
+import de.uniks.stp.wedoit.accord.client.controller.OptionsScreenController;
+import de.uniks.stp.wedoit.accord.client.controller.WelcomeScreenController;
 import de.uniks.stp.wedoit.accord.client.model.LocalUser;
 import de.uniks.stp.wedoit.accord.client.network.RestClient;
 import javafx.application.Application;
@@ -18,6 +20,8 @@ public class StageManager extends Application {
     private static LoginScreenController loginScreenController;
     private static MainScreenController mainScreenController;
     private static RestClient restClient;
+    private static WelcomeScreenController welcomeScreenController;
+    private static OptionsScreenController optionsScreenController;
 
     @Override
     public void start(Stage primaryStage) {
@@ -59,7 +63,7 @@ public class StageManager extends Application {
             stage.setTitle("Login");
             stage.setScene(scene);
             stage.centerOnScreen();
-            
+
             stage.setResizable(false);
 
         } catch (Exception e) {
@@ -95,6 +99,22 @@ public class StageManager extends Application {
     }
 
     public static void showWelcomeScreen() {
+        cleanup();
+
+        try {
+            Parent root = FXMLLoader.load(StageManager.class.getResource("view/WelcomeScreen.fxml"));
+            Scene scene = new Scene(root);
+
+            welcomeScreenController = new WelcomeScreenController(root, model, editor);
+            welcomeScreenController.init();
+
+            stage.setTitle("Welcome");
+            stage.setScene(scene);
+            stage.centerOnScreen();
+        } catch (Exception e) {
+            System.err.println("Error on showing WelcomeScreen");
+            e.printStackTrace();
+        }
     }
 
     public static void showServerScreen() {
@@ -102,7 +122,22 @@ public class StageManager extends Application {
     }
 
     public static void showOptionsScreen() {
+        cleanup();
 
+        try {
+            Parent root = FXMLLoader.load(StageManager.class.getResource("view/OptionsScreen.fxml"));
+            Scene scene = new Scene(root);
+
+            optionsScreenController = new OptionsScreenController(root, model, editor);
+            optionsScreenController.init();
+
+            stage.setTitle("Options");
+            stage.setScene(scene);
+            stage.centerOnScreen();
+        } catch (Exception e) {
+            System.err.println("Error on showing OptionsScreen");
+            e.printStackTrace();
+        }
     }
 
     private static void cleanup() {
@@ -113,6 +148,14 @@ public class StageManager extends Application {
         if (mainScreenController != null) {
             mainScreenController.stop();
             mainScreenController = null;
+        }
+        if (welcomeScreenController != null) {
+            welcomeScreenController.stop();
+            welcomeScreenController = null;
+        }
+        if (optionsScreenController != null) {
+            optionsScreenController.stop();
+            optionsScreenController = null;
         }
     }
 
