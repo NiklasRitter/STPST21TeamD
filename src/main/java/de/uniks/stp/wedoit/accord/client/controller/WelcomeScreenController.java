5d18c4e8c3b3406ac06a5acb9ab7672c618ddb52
NiklasRitter <n.ritter2900@gmail.com>
2021-05-03 15:57:23 +0200

    feat: Welcome Screen btnHome, btnOptions implemented

--- src/main/java/de/uniks/stp/wedoit/accord/client/controller/WelcomeScreenController.java
+++ src/main/java/de/uniks/stp/wedoit/accord/client/controller/WelcomeScreenController.java
@@ -1,14 +1,20 @@
 package de.uniks.stp.wedoit.accord.client.controller;
 
 import de.uniks.stp.wedoit.accord.client.Editor;
+import de.uniks.stp.wedoit.accord.client.StageManager;
 import de.uniks.stp.wedoit.accord.client.model.LocalUser;
+import javafx.event.ActionEvent;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
 
 public class WelcomeScreenController {
 
     private Parent view;
     private LocalUser localUser;
     private Editor editor;
+    private Button btnOptions;
+    private Button btnHome;
+    private Button btnLogout;
 
     public WelcomeScreenController(Parent view, LocalUser model, Editor editor) {
         this.view = view;
@@ -17,9 +23,51 @@ public class WelcomeScreenController {
     }
 
     public void init() {
+
+        this.btnOptions = (Button) view.lookup("#btnOptions");
+        this.btnHome = (Button) view.lookup("#btnHome");
+        this.btnLogout = (Button) view.lookup("#btnLogout");
+
+        this.btnHome.setOnAction(this::btnHomeOnClicked);
+        this.btnLogout.setOnAction(this::btnLogoutOnClicked);
+        this.btnOptions.setOnAction(this::btnOptionsOnClicked);
+
     }
 
     public void stop() {
+        this.btnHome.setOnAction(null);
+        this.btnLogout.setOnAction(null);
+        this.btnOptions.setOnAction(null);
+
+        this.btnOptions = null;
+        this.btnHome = null;
+        this.btnLogout = null;
     }
 
+    /**
+     * redirect to Main Screen
+     *
+     * @param actionEvent
+     */
+    private void btnHomeOnClicked(ActionEvent actionEvent) {
+        StageManager.showMainScreen();
+    }
+
+    /**
+     * logout current LocalUser and redirect to the LoginScreen
+     *
+     * @param actionEvent
+     */
+    private void btnLogoutOnClicked(ActionEvent actionEvent) {
+        //TODO implement me
+    }
+
+    /**
+     * redirect to Options Menu
+     *
+     * @param actionEvent
+     */
+    private void btnOptionsOnClicked(ActionEvent actionEvent) {
+        StageManager.showOptionsScreen();
+    }
 }
