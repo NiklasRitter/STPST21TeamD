a0f919a64d2447f152bb38a1b2ef8153f8294b5f
Alex <alexander-rode1@web.de>
2021-05-06 00:45:35 +0200

    feat: Mock restClient getServers method and add responses
    add help methods to have responses for the mocked rest client

--- src/test/java/de/uniks/stp/wedoit/accord/client/controller/MainScreenControllerTest.java
+++ src/test/java/de/uniks/stp/wedoit/accord/client/controller/MainScreenControllerTest.java
@@ -17,6 +17,13 @@ import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import org.testfx.framework.junit.ApplicationTest;
 
+import javax.json.Json;
+import javax.json.JsonObject;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 
 public class MainScreenControllerTest extends ApplicationTest {
 
@@ -54,6 +61,61 @@ public class MainScreenControllerTest extends ApplicationTest {
     @BeforeEach
     public void setup() {
         MockitoAnnotations.openMocks(this);
+    }
+
+    /**
+     * Mock the rest client's getServers method and create a callback
+     *
+     * @param json JsonObject, which one should return from the rest client as JsonNode
+     */
+    public void mockRestClient(JsonObject json) {
+        when(res.getBody()).thenReturn(new JsonNode(json.toString()));
+
+        verify(restMock).getServers(anyString(), callbackArgumentCaptor.capture());
+
+        Callback<JsonNode> callback = callbackArgumentCaptor.getValue();
+        callback.completed(res);
+    }
+
+    // Help methods to create response for mocked rest client
+
+    /**
+     * create a getServers response with two servers with id an name:
+     * <b>{"id":"5e2ffbd8770dd077d03df505","name":"BMainTestServerOne"},
+     * {"id":"5e2ffbd8770dd077d03df506","name":"AMainTestServerTwo"}</b>
+     */
+    public JsonObject buildGetServersSuccessWithTwoServers() {
+        return Json.createObjectBuilder()
+                .add("status", "success").add("message", "")
+                .add("data", Json.createArrayBuilder()
+                        .add(Json.createObjectBuilder()
+                                .add("id", "5e2ffbd8770dd077d03df505")
+                                .add("name", "BMainTestServerOne")
+                        )
+                        .add(Json.createObjectBuilder()
+                                .add("id", "5e2ffbd8770dd077d03df506")
+                                .add("name", "AMainTestServerTwo"))
+                ).build();
+    }
+
+    /**
+     * create a getServers response with zero servers
+     */
+    public JsonObject buildGetServersSuccessWithZeroServers() {
+        return Json.createObjectBuilder()
+                .add("status", "success").add("message", "")
+                .add("data", Json.createArrayBuilder()
+                ).build();
+    }
+
+    /**
+     * create a getServers response with a failure status
+     */
+    public JsonObject buildGetServersFailureResponse() {
+        return Json.createObjectBuilder()
+                .add("status", "failure").add("message", "Log in first")
+                .add("data", Json.createObjectBuilder()
+                ).build();
     }
 
 }
