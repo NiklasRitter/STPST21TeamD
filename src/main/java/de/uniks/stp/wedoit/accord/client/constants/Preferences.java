1827e5b2a4996a9946f82ab02f838fb60fbee3d4
Philip Laskowicz <philip.laskowicz@web.de>
2021-06-09 14:10:10 +0200

    feat: Add Contants for remembering user login

--- src/main/java/de/uniks/stp/wedoit/accord/client/constants/Preferences.java
+++ src/main/java/de/uniks/stp/wedoit/accord/client/constants/Preferences.java
@@ -2,4 +2,7 @@ package de.uniks.stp.wedoit.accord.client.constants;
 
 public class Preferences {
     public static final String DARKMODE = "darkmode";
+    public static final String REMEMBER_ME = "remember_me";
+    public static final String USERNAME = "username";
+    public static final String PASSWORD = "password";
 }
