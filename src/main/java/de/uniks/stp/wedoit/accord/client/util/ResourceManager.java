9d054caa6b6fee728a9cdff0024051793a60b772
Philip Laskowicz <philip.laskowicz@web.de>
2021-06-14 01:22:36 +0200

    feat: Add updating of propertyChangeListener, logic for automatic login

--- src/main/java/de/uniks/stp/wedoit/accord/client/util/ResourceManager.java
+++ src/main/java/de/uniks/stp/wedoit/accord/client/util/ResourceManager.java
@@ -1,9 +1,86 @@
 package de.uniks.stp.wedoit.accord.client.util;
 
+import de.uniks.stp.wedoit.accord.client.model.AccordClient;
+import de.uniks.stp.wedoit.accord.client.model.LocalUser;
 import de.uniks.stp.wedoit.accord.client.model.Options;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.Objects;
 
 public class ResourceManager {
     private final PreferenceManager preferenceManager = new PreferenceManager();
+    public PropertyChangeListener localUserListener = this::onLocalUserChange;
+
+    /**
+     * Add all necessary propertyChangeListeners from options.
+     *
+     * @param clientModel The model of the Client.
+     */
+    public void start(AccordClient clientModel) {
+        if (clientModel.getOptions() != null) {
+            loadOptions(clientModel.getOptions());
+            Objects.requireNonNull(clientModel.getOptions()).listeners().addPropertyChangeListener(Options.PROPERTY_DARKMODE,
+                    preferenceManager.darkmodeListener);
+            Objects.requireNonNull(clientModel.getOptions()).listeners().addPropertyChangeListener(Options.PROPERTY_DARKMODE, preferenceManager.rememberMeListener);
+        }
+        if (clientModel.getLocalUser() != null) {
+            loadLocalUser(clientModel.getLocalUser());
+            Objects.requireNonNull(clientModel.getLocalUser()).listeners().addPropertyChangeListener(LocalUser.PROPERTY_PASSWORD,
+                    preferenceManager.passwordListener);
+            Objects.requireNonNull(clientModel.getLocalUser()).listeners().addPropertyChangeListener(LocalUser.PROPERTY_NAME,
+                    preferenceManager.usernameListener);
+        }
+        Objects.requireNonNull(clientModel).listeners().addPropertyChangeListener(AccordClient.PROPERTY_LOCAL_USER,
+                localUserListener);
+    }
+
+    /**
+     * Remove all propertyChangeListeners from options.
+     *
+     * @param clientModel The model of the Client.
+     */
+    public void stop(AccordClient clientModel) {
+        if (clientModel.getOptions() != null) {
+            Objects.requireNonNull(clientModel.getOptions()).listeners().removePropertyChangeListener(Options.PROPERTY_DARKMODE,
+                    preferenceManager.darkmodeListener);
+            Objects.requireNonNull(clientModel.getOptions()).listeners().removePropertyChangeListener(Options.PROPERTY_DARKMODE, preferenceManager.rememberMeListener);
+        }
+        if (clientModel.getLocalUser() != null) {
+            Objects.requireNonNull(clientModel.getLocalUser()).listeners().removePropertyChangeListener(LocalUser.PROPERTY_PASSWORD,
+                    preferenceManager.passwordListener);
+            Objects.requireNonNull(clientModel.getLocalUser()).listeners().removePropertyChangeListener(LocalUser.PROPERTY_NAME,
+                    preferenceManager.usernameListener);
+        }
+        Objects.requireNonNull(clientModel).listeners().removePropertyChangeListener(AccordClient.PROPERTY_LOCAL_USER,
+                localUserListener);
+    }
+
+    /**
+     * Called when the local user of the AccordClient is changed.
+     * <p>
+     * Updates the propertyChangeListeners.
+     *
+     * @param propertyChangeEvent The called event.
+     */
+    public void onLocalUserChange(PropertyChangeEvent propertyChangeEvent) {
+        if (propertyChangeEvent.getOldValue() instanceof LocalUser) {
+            LocalUser oldLocalUser = (LocalUser) propertyChangeEvent.getOldValue();
+
+            oldLocalUser.listeners().removePropertyChangeListener(LocalUser.PROPERTY_PASSWORD,
+                    preferenceManager.passwordListener);
+            oldLocalUser.listeners().removePropertyChangeListener(LocalUser.PROPERTY_NAME,
+                    preferenceManager.usernameListener);
+        }
+        if (propertyChangeEvent.getNewValue() instanceof LocalUser) {
+            LocalUser newLocalUser = (LocalUser) propertyChangeEvent.getNewValue();
+
+            newLocalUser.listeners().addPropertyChangeListener(LocalUser.PROPERTY_PASSWORD,
+                    preferenceManager.passwordListener);
+            newLocalUser.listeners().addPropertyChangeListener(LocalUser.PROPERTY_NAME,
+                    preferenceManager.usernameListener);
+        }
+    }
 
     /**
      * Save all options using the PreferenceManager.
@@ -20,24 +97,30 @@ public class ResourceManager {
      * <p>
      * Add necessary PropertyChangeListener.
      *
-     * @return The loaded options.
+     * @param options The Options the values should be loaded into.
      */
-    public Options loadOptions() {
-        Options options = new Options();
-        options.setDarkmode(preferenceManager.loadDarkmode());
-        options.listeners().addPropertyChangeListener(Options.PROPERTY_DARKMODE, preferenceManager.darkmodeListener);
-        options.setRememberMe(preferenceManager.loadRememberMe());
-        options.listeners().addPropertyChangeListener(Options.PROPERTY_DARKMODE, preferenceManager.rememberMeListener);
-        return options;
+    public void loadOptions(Options options) {
+        Objects.requireNonNull(options).setDarkmode(preferenceManager.loadDarkmode());
+        Objects.requireNonNull(options).setRememberMe(preferenceManager.loadRememberMe());
     }
 
     /**
-     * Remove all propertyChangeListeners from options.
+     * Save username and password using the PreferenceManager.
      *
-     * @param options The options of the Client.
+     * @param localUser The localUser to be saved.
      */
-    public void stop(Options options) {
-        options.listeners().removePropertyChangeListener(Options.PROPERTY_DARKMODE, preferenceManager.darkmodeListener);
-        options.listeners().removePropertyChangeListener(Options.PROPERTY_DARKMODE, preferenceManager.rememberMeListener);
+    public void saveLocalUser(LocalUser localUser) {
+        preferenceManager.savePassword(localUser.getPassword());
+        preferenceManager.saveUsername(localUser.getName());
+    }
+
+    /**
+     * Load username and password using the PreferenceManager.
+     *
+     * @param localUser The localUser the values should be loaded into.
+     */
+    public void loadLocalUser(LocalUser localUser) {
+        Objects.requireNonNull(localUser).setPassword(preferenceManager.loadPassword());
+        Objects.requireNonNull(localUser).setName(preferenceManager.loadUsername());
     }
 }
