76cade607feaf4c4ac9955098a48e9e06b53db82
Philip Laskowicz <philip.laskowicz@web.de>
2021-06-16 22:12:06 +0200

    feat: Add preference for InitializationVector

--- src/main/java/de/uniks/stp/wedoit/accord/client/constants/Preferences.java
+++ src/main/java/de/uniks/stp/wedoit/accord/client/constants/Preferences.java
@@ -5,4 +5,5 @@ public class Preferences {
     public static final String REMEMBER_ME = "remember_me";
     public static final String USERNAME = "username";
     public static final String PASSWORD = "password";
+    public static final String INITIALIZATION_VECTOR = "initialization_vector";
 }
