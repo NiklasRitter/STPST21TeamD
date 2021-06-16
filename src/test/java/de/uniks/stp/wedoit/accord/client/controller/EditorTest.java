7484cc90834719a2a7db5d252fa53bd450c5a077
Amir <amzi@student.uni-kassel.de>
2021-06-16 13:40:31 +0200

    feat: tests HaveServer and CurrentServer in Editor

--- src/test/java/de/uniks/stp/wedoit/accord/client/controller/EditorTest.java
+++ src/test/java/de/uniks/stp/wedoit/accord/client/controller/EditorTest.java
@@ -4,26 +4,41 @@ import de.uniks.stp.wedoit.accord.client.Editor;
 
 import de.uniks.stp.wedoit.accord.client.model.AccordClient;
 import de.uniks.stp.wedoit.accord.client.model.LocalUser;
+import de.uniks.stp.wedoit.accord.client.model.Server;
 import org.junit.*;
 
 public class EditorTest {
 
     private Editor editor;
+    private LocalUser localUser;
+    private Server server;
     @Before
     public void initEditor() {
         editor = new Editor();
+        editor.haveAccordClient();
+        editor.haveLocalUser();
+        localUser = new LocalUser();
+        server = new Server();
     }
 
     @Test
     public void testHaveLocalUser() {
-        editor.haveAccordClient();
-        editor.haveLocalUser();
 
-        LocalUser localUser = editor.haveLocalUser("Amir", "testKey123");
+        localUser = editor.haveLocalUser("Amir", "testKey123");
 
         Assert.assertEquals(editor.getLocalUser().getId(), localUser.getId());
         Assert.assertEquals(editor.getLocalUser().getUserKey(), "testKey123");
         Assert.assertEquals(editor.getLocalUser().getName(), "Amir");
+    }
+
+    @Test
+    public void testHaveServer() {
+        server = editor.haveServer(localUser, "0098", "Accord");
+        editor.setCurrentServer(server);
+
+        Assert.assertNotNull(editor.getCurrentServer());
+        Assert.assertEquals(server.getName(), "Accord");
+        Assert.assertEquals(server, editor.getCurrentServer());
     }
 
 }
