d047fccb6a7c3dd1a32d5826c53206c0948cbb92
Amir <amzi@student.uni-kassel.de>
2021-06-18 00:07:31 +0200

    feat: Tests parse method in JsonUtil

--- src/test/java/de/uniks/stp/wedoit/accord/client/util/JsonUtilTest.java
+++ src/test/java/de/uniks/stp/wedoit/accord/client/util/JsonUtilTest.java
@@ -1,14 +1,50 @@
 package de.uniks.stp.wedoit.accord.client.util;
 
+import de.uniks.stp.wedoit.accord.client.Editor;
+import de.uniks.stp.wedoit.accord.client.controller.NetworkController;
+import de.uniks.stp.wedoit.accord.client.model.LocalUser;
+import de.uniks.stp.wedoit.accord.client.model.Server;
+import de.uniks.stp.wedoit.accord.client.model.User;
+import de.uniks.stp.wedoit.accord.client.network.RestClient;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
+
+import javax.json.JsonObject;
+
+import static de.uniks.stp.wedoit.accord.client.constants.JSON.*;
 
 public class JsonUtilTest {
 
     private JsonUtil jsonUtil;
+    private RestClient restClient;
+    private Editor editor;
+    private User user;
+    private LocalUser localUser;
+    private Server server;
 
     @Before
     public void initJsonUtil() {
         jsonUtil = new JsonUtil();
+        restClient = new RestClient();
+        editor = new Editor();
+        editor.haveAccordClient();
+        editor.haveLocalUser();
+        server = new Server();
+        user = new User().setName("Gelareh").setId("021");
+        localUser = new LocalUser().setName("Amir").setUserKey("testKey123").setId("1364");
     }
-    
+
+    @Test
+    public void testParse() {
+        String stringJson = "{\"data\":{\"name\":\"Iran\",\"id\":\"0098\"},\"message\":\"\",\"status\":\"success\"}";
+
+        JsonObject createServerAnswer = JsonUtil.parse(stringJson);
+
+        Assert.assertEquals(createServerAnswer.getString(MESSAGE), "");
+        Assert.assertEquals(createServerAnswer.getString(STATUS), "success");
+        Assert.assertEquals(createServerAnswer.getJsonObject(DATA).getString(NAME), "Iran");
+        Assert.assertEquals(createServerAnswer.getJsonObject(DATA).getString(ID), "0098");
+    }
+
 }
