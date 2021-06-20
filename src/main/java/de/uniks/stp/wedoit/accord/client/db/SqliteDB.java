d2c930ebf1360e3997ec550895cd0205448c0ee6
Philip Laskowicz <philip.laskowicz@web.de>
2021-06-20 15:59:12 +0200

    feat: Achnage loading order and timestamp to offset

--- src/main/java/de/uniks/stp/wedoit/accord/client/db/SqliteDB.java
+++ src/main/java/de/uniks/stp/wedoit/accord/client/db/SqliteDB.java
@@ -148,18 +148,19 @@ public class SqliteDB {
      * Query for messages between username (set in constructor) and user, ordered by timestamp
      *
      * @param user username to find chats with
+     * @param offset The offset from the start
      * @return list of PrivateMessage that the localUser had with user
      */
-    public List<PrivateMessage> getLastFiftyMessagesBetweenUsers(String user, long timestamp) {
+    public List<PrivateMessage> getLastFiftyMessagesBetweenUsers(String user, int offset) {
         List<PrivateMessage> messages = new ArrayList<>();
         try (Connection conn = DriverManager.getConnection(url + username + ".sqlite");
              PreparedStatement prep = conn.prepareStatement("SELECT * FROM messages WHERE ((sender = ? AND receiver =" +
-                     " ?) OR (sender = ? AND receiver = ?)) AND times < ? ORDER BY times LIMIT 50")) {
+                     " ?) OR (sender = ? AND receiver = ?)) ORDER BY times DESC LIMIT ?, 50")) {
             prep.setString(1, user);
             prep.setString(2, username);
             prep.setString(3, username);
             prep.setString(4, user);
-            prep.setLong(5, timestamp);
+            prep.setInt(5, offset);
 
             ResultSet rs = prep.executeQuery();
 
@@ -189,7 +190,7 @@ public class SqliteDB {
         List<PrivateMessage> messages = new ArrayList<>();
         try (Connection conn = DriverManager.getConnection(url + username + ".sqlite");
              PreparedStatement prep = conn.prepareStatement("SELECT * FROM messages WHERE ((sender = ? AND receiver =" +
-                     " ?) OR (sender = ? AND receiver = ?)) ORDER BY times LIMIT 50")) {
+                     " ?) OR (sender = ? AND receiver = ?)) ORDER BY times DESC LIMIT 50")) {
             prep.setString(1, user);
             prep.setString(2, username);
             prep.setString(3, username);
