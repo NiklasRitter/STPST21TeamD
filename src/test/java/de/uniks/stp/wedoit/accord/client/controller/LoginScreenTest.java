fdc41c1e1d1663f5de0b8c9331deed7e5383e6fb
Amir <amzi@student.uni-kassel.de>
2021-06-30 14:08:58 +0200

    fix: Formats and cleans up the code here

--- src/test/java/de/uniks/stp/wedoit/accord/client/controller/LoginScreenTest.java
+++ src/test/java/de/uniks/stp/wedoit/accord/client/controller/LoginScreenTest.java
@@ -199,8 +199,8 @@ public class LoginScreenTest extends ApplicationTest {
         when(resGuestLogin.getBody()).thenReturn(new JsonNode(returnMessage.toString()));
 
         //Shows that the labels for guest user data are empty
-        Assert.assertEquals("", ((Label)lookup("#lblGuestPassword").query()).getText());
-        Assert.assertEquals("", ((Label)lookup("#lblUserValid").query()).getText());
+        Assert.assertEquals("", ((Label) lookup("#lblGuestPassword").query()).getText());
+        Assert.assertEquals("", ((Label) lookup("#lblUserValid").query()).getText());
 
         clickOn("#btnGuestLogin");
 
@@ -215,13 +215,12 @@ public class LoginScreenTest extends ApplicationTest {
 
         //Shows that the data will be updated after creating the guest user via Guest Login button.
         WaitForAsyncUtils.waitForFxEvents();
-        Assert.assertEquals("This user is valid for 24 hours", ((Label)lookup("#lblUserValid").query()).getText());
-        Assert.assertEquals("with the password: Amir Ziaiyan", ((Label)lookup("#lblGuestPassword").query()).getText());
+        Assert.assertEquals("This user is valid for 24 hours", ((Label) lookup("#lblUserValid").query()).getText());
+        Assert.assertEquals("with the password: Amir Ziaiyan", ((Label) lookup("#lblGuestPassword").query()).getText());
         Assert.assertEquals("success", resGuestLogin.getBody().getObject().getString(STATUS));
         Assert.assertEquals("", resGuestLogin.getBody().getObject().getString(MESSAGE));
         Assert.assertEquals("Amir Ziaiyan", resGuestLogin.getBody().getObject().getJSONObject(DATA).getString(NAME));
         Assert.assertEquals("Amir Ziaiyan", resGuestLogin.getBody().getObject().getJSONObject(DATA).getString(PASSWORD));
-
     }
 
 
