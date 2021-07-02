1bf9025a7fcd4978aa6c70c3e0544ba5f10d041d
Amir <amzi@student.uni-kassel.de>
2021-07-02 16:27:03 +0200

    feat: Tests LanguagePrefrences.java & LanguageResolver.java

---
+++ src/test/java/de/uniks/stp/wedoit/accord/client/language/LanguagePreferencesAndLanguageResolverTest.java
@@ -0,0 +1,38 @@
+package de.uniks.stp.wedoit.accord.client.language;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Locale;
+
+public class LanguagePreferencesAndLanguageResolverTest {
+
+    @Before
+    public void LanguagePreferencesAndLanguageResolver() {
+        Locale.setDefault(LanguagePreferences.getLanguagePreferences().getCurrentLocale("language/Language"));
+        LanguageResolver.load();
+    }
+
+    @Test
+    public void testGetCurrentLocale() {
+        Locale locale = LanguagePreferences.getLanguagePreferences().getCurrentLocale("language/Language_fa_IR");
+
+        Assert.assertEquals(locale.getLanguage(), "fa_ir");
+    }
+
+    @Test
+    public void testGetString() {
+        Assert.assertEquals(LanguageResolver.getString("NEW_SERVERNAME"), "New server name");
+        Assert.assertEquals(Locale.getDefault().getLanguage(), "en_gb");
+    }
+
+    @Test
+    public void testLoad() {
+        Locale.setDefault(LanguagePreferences.getLanguagePreferences().getCurrentLocale("language/Language_fa_IR"));
+        LanguageResolver.load();
+        Assert.assertEquals(LanguageResolver.getString("NEW_SERVERNAME"), "نام سرور جدید");
+        Assert.assertEquals(Locale.getDefault().getLanguage(), "fa_ir");
+    }
+
+}
