b3ddea61144870b2127955cb2a52f137b6e161dd
Amir <amzi@student.uni-kassel.de>
2021-07-15 21:41:02 +0200

    feat: Tests ControllerNames.java

--- src/test/java/de/uniks/stp/wedoit/accord/client/constants/ConstantsClassesTest.java
+++ src/test/java/de/uniks/stp/wedoit/accord/client/constants/ConstantsClassesTest.java
@@ -1,2 +1,14 @@
-package de.uniks.stp.wedoit.accord.client.constants;public class ConstantsClassesTest {
+package de.uniks.stp.wedoit.accord.client.constants;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ConstantsClassesTest {
+
+    @Test
+    public void testControllerNames() {
+        ControllerNames controllerNames = new ControllerNames();
+        Assert.assertEquals(controllerNames.LOGIN_SCREEN_CONTROLLER, "loginScreenController");
+    }
+
 }
