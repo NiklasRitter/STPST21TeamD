fb94d839bd02aab75f5524f82960e1b4064e399d
NiklasRitter <n.ritter2900@gmail.com>
2021-07-15 12:09:52 +0200

    fix: audio manager tests

--- src/test/java/de/uniks/stp/wedoit/accord/client/util/AudioManagerTest.java
+++ src/test/java/de/uniks/stp/wedoit/accord/client/util/AudioManagerTest.java
@@ -1,6 +1,5 @@
 package de.uniks.stp.wedoit.accord.client.util;
 
-import de.uniks.stp.wedoit.accord.client.Editor;
 import de.uniks.stp.wedoit.accord.client.StageManager;
 import de.uniks.stp.wedoit.accord.client.model.*;
 import de.uniks.stp.wedoit.accord.client.network.RestClient;
@@ -29,12 +28,11 @@ import org.testfx.util.WaitForAsyncUtils;
 import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonObject;
-
+import java.net.DatagramSocket;
 import java.util.concurrent.TimeUnit;
 
 import static de.uniks.stp.wedoit.accord.client.constants.ControllerNames.SERVER_SCREEN_CONTROLLER;
 import static de.uniks.stp.wedoit.accord.client.constants.Network.*;
-import static de.uniks.stp.wedoit.accord.client.constants.Network.AND_SERVER_ID_URL;
 import static de.uniks.stp.wedoit.accord.client.constants.Stages.STAGE;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
@@ -42,7 +40,6 @@ import static org.mockito.Mockito.*;
 public class AudioManagerTest extends ApplicationTest {
 
     private Stage stage;
-    private Stage emojiPickerStage;
     private StageManager stageManager;
     private LocalUser localUser;
     private Server server;
@@ -88,7 +85,6 @@ public class AudioManagerTest extends ApplicationTest {
         stageManager.getResourceManager().saveOptions(new Options().setLanguage("en_GB"));
 
         this.stageManager.start(stage);
-        this.emojiPickerStage = this.stageManager.getEmojiPickerStage();
         //create localUser to skip the login screen and create server to skip the MainScreen
         this.localUser = this.stageManager.getEditor().haveLocalUser("JohnDoe", "testKey123");
         this.localUser.setPassword("secret").setId("123");
@@ -111,7 +107,6 @@ public class AudioManagerTest extends ApplicationTest {
         webSocketClient = null;
         chatWebSocketClient = null;
         stage = null;
-        emojiPickerStage = null;
         stageManager = null;
         localUser = null;
         server = null;
@@ -147,7 +142,7 @@ public class AudioManagerTest extends ApplicationTest {
         this.wsCallback.handleMessage(webSocketJson);
     }
 
-    public void mockAudioInit(){
+    public void mockAudioInit() {
         verify(audioConnection).startConnection("cranberry.uniks.de", 33100);
     }
 
@@ -171,14 +166,14 @@ public class AudioManagerTest extends ApplicationTest {
         callback.completed(res);
     }
 
-    public void mockJoinAudio(JsonObject restClientJson){
+    public void mockJoinAudio(JsonObject restClientJson) {
         when(res.getBody()).thenReturn(new JsonNode(restClientJson.toString()));
         verify(restMock).joinAudioChannel(anyString(), anyString(), anyString(), anyString(), channelCallbackArgumentCaptor.capture());
         Callback<JsonNode> callback = channelCallbackArgumentCaptor.getValue();
         callback.completed(res);
     }
 
-    public void mockLeaveAudio(JsonObject restClientJson){
+    public void mockLeaveAudio(JsonObject restClientJson) {
         when(res.getBody()).thenReturn(new JsonNode(restClientJson.toString()));
         verify(restMock).leaveAudioChannel(anyString(), anyString(), anyString(), anyString(), channelCallbackArgumentCaptor.capture());
         Callback<JsonNode> callback = channelCallbackArgumentCaptor.getValue();
@@ -186,7 +181,7 @@ public class AudioManagerTest extends ApplicationTest {
     }
 
     @Test
-    public void joinAudioServerTest(){
+    public void joinAudioServerTest() {
         initUserListView();
         JsonArray audioMembers = Json.createArrayBuilder().add("I1").build();
         initAudioChannelListView(audioMembers);
@@ -208,7 +203,7 @@ public class AudioManagerTest extends ApplicationTest {
     }
 
     @Test
-    public void leaveAudioChannelTest(){
+    public void leaveAudioChannelTest() {
         initUserListView();
         JsonArray audioMembers = Json.createArrayBuilder().add("I1").build();
         initAudioChannelListView(audioMembers);
@@ -228,11 +223,23 @@ public class AudioManagerTest extends ApplicationTest {
     }
 
     @Test
-    public void muteAndUnmuteUserAudioTest(){
+    public void muteAndUnmuteUserAudioTest() {
         joinAudioServerTest();
         AudioManager audioManager = stageManager.getEditor().getAudioManager();
         Channel channel = stageManager.getEditor().getChannelById(server, "idTest", "idTest1");
-        AudioConnection tempAudioCon = new AudioConnection(localUser, channel);
+        AudioConnection tempAudioCon = new AudioConnection(localUser, channel) {
+            @Override
+            protected DatagramSocket createSocket() {
+                DatagramSocket datagramSocket = null;
+                try {
+                    datagramSocket = new DatagramSocket(33100);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return datagramSocket;
+            }
+        };
+
         audioManager.setAudioConnection(tempAudioCon);
         tempAudioCon.startConnection("localhost", 33100);
         WaitForAsyncUtils.sleep(500, TimeUnit.MILLISECONDS);
@@ -244,35 +251,34 @@ public class AudioManagerTest extends ApplicationTest {
         tempAudioCon.close();
     }
 
-    public AudioConnection secondAudioConnection() {
-        LocalUser user = new LocalUser().setName("Bill");
-        Channel channel = stageManager.getEditor().getChannelById(server, "idTest", "idTest1");
-        AudioManager audioManager = new AudioManager(new Editor());
-        AudioConnection tempAudioCon2 = new AudioConnection(user, channel);
-        audioManager.setAudioConnection(tempAudioCon2);
-        tempAudioCon2.startConnection("localhost", 33100);
-        WaitForAsyncUtils.sleep(500, TimeUnit.MILLISECONDS);
-
-        return tempAudioCon2;
-    }
-
     @Test
-    public void muteAndUnmuteAllTest(){
+    public void muteAndUnmuteAllTest() {
         joinAudioServerTest();
         Channel channel = stageManager.getEditor().getChannelById(server, "idTest", "idTest1");
-        AudioConnection tempAudioCon = new AudioConnection(localUser, channel);
+        AudioConnection tempAudioCon = new AudioConnection(localUser, channel) {
+            @Override
+            protected DatagramSocket createSocket() {
+                DatagramSocket datagramSocket = null;
+                try {
+                    datagramSocket = new DatagramSocket(33100);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return datagramSocket;
+            }
+        };
         stageManager.getEditor().getAudioManager().setAudioConnection(tempAudioCon);
         tempAudioCon.startConnection("localhost", 33100);
         WaitForAsyncUtils.sleep(500, TimeUnit.MILLISECONDS);
         clickOn("#btnMuteAll");
-        for(User user : channel.getAudioMembers()){
-            if(!user.getName().equals(localUser.getName())){
+        for (User user : channel.getAudioMembers()) {
+            if (!user.getName().equals(localUser.getName())) {
                 Assert.assertTrue(user.isMuted());
             }
         }
         clickOn("#btnMuteAll");
-        for(User user : channel.getAudioMembers()){
-            if(!user.getName().equals(localUser.getName())){
+        for (User user : channel.getAudioMembers()) {
+            if (!user.getName().equals(localUser.getName())) {
                 Assert.assertFalse(user.isMuted());
             }
         }
@@ -280,12 +286,23 @@ public class AudioManagerTest extends ApplicationTest {
     }
 
     @Test
-    public void muteAndUnmuteYourselfTest(){
+    public void muteAndUnmuteYourselfTest() {
         joinAudioServerTest();
         Channel channel = stageManager.getEditor().getChannelById(server, "idTest", "idTest1");
-        AudioConnection tempAudioCon = new AudioConnection(localUser, channel);
+        AudioConnection tempAudioCon = new AudioConnection(localUser, channel) {
+            @Override
+            protected DatagramSocket createSocket() {
+                DatagramSocket datagramSocket = null;
+                try {
+                    datagramSocket = new DatagramSocket(33100);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return datagramSocket;
+            }
+        };
         stageManager.getEditor().getAudioManager().setAudioConnection(tempAudioCon);
-        tempAudioCon.startConnection("localhost" , 33100);
+        tempAudioCon.startConnection("localhost", 33100);
         WaitForAsyncUtils.sleep(500, TimeUnit.MILLISECONDS);
         stageManager.getEditor().getAudioManager().muteYourself(localUser);
         Assert.assertTrue(localUser.isMuted());
@@ -371,6 +388,4 @@ public class AudioManagerTest extends ApplicationTest {
         return Json.createObjectBuilder().add("action", "userJoined").add("data",
                 Json.createObjectBuilder().add("id", "123456").add("name", "Phil")).build();
     }
-
-
 }
