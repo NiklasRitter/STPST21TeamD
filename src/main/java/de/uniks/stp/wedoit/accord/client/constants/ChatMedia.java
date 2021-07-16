ba0836e24afd7e0d8b78ddd023a7e6cd5479074e
Amir <amzi@student.uni-kassel.de>
2021-07-16 16:02:12 +0200

    feat: Adds MP3 key

--- src/main/java/de/uniks/stp/wedoit/accord/client/constants/ChatMedia.java
+++ src/main/java/de/uniks/stp/wedoit/accord/client/constants/ChatMedia.java
@@ -7,6 +7,7 @@ public class ChatMedia {
     public static final String SUPPORTED_CLOUD = "drive.google.com";
     public static final String YT_WATCH = "youtube.com/watch?v=";
     public static final String MP4 = ".mp4";
+    public static final String MP3 = ".mp3";
     public static final String GIF = ".gif";
     public static final String YT_SHORT = "youtu.be/";
     public static final String YT_PREFIX = "https://www.youtube.com/watch?v=";
