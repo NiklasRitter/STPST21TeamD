64d57718a997bac428862cb81cdfa67027d717ab
Alex <alexander-rode1@web.de>
2021-07-25 15:42:08 +0200

    feat: Add description to users in the model

--- src/gen/java/de/uniks/stp/wedoit/accord/client/model/GenModel.java
+++ src/gen/java/de/uniks/stp/wedoit/accord/client/model/GenModel.java
@@ -30,6 +30,7 @@ public class GenModel implements ClassModelDecorator {
     }
 
     class LocalUser {
+        String description;
         String password;
         String name;
         String userKey;
@@ -51,6 +52,7 @@ public class GenModel implements ClassModelDecorator {
     }
 
     class User {
+        String description;
         String name;
         boolean onlineStatus;
         boolean chatRead;
