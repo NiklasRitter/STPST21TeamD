2a0885c3b7ddbff3e24bbe030e77638f2aa1fd36
Philip Laskowicz <philip.laskowicz@web.de>
2021-08-01 13:02:46 +0200

    feat: Add Constants for User Descrptions

---
+++ src/main/java/de/uniks/stp/wedoit/accord/client/constants/UserDescription.java
@@ -0,0 +1,12 @@
+package de.uniks.stp.wedoit.accord.client.constants;
+
+/**
+ * includes constants to create and handle incoming jsons
+ */
+public class UserDescription {
+    public static final String SPOTIFY = "#";
+    public static final String GITHUB = "%";
+    public static final String STEAM = "?";
+    public static final String CUSTOM = "+";
+    public static final String CLUB_PENGUIN = "=";
+}
