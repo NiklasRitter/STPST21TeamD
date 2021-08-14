5c5a353af04b9a445cc3b3de45898db21351674c
Alex <alexander-rode1@web.de>
2021-08-14 23:17:39 +0200

    feat: Load description for the local user

--- src/main/java/de/uniks/stp/wedoit/accord/client/controller/subcontroller/optionsSubController/DescriptionController.java
+++ src/main/java/de/uniks/stp/wedoit/accord/client/controller/subcontroller/optionsSubController/DescriptionController.java
@@ -3,6 +3,7 @@ package de.uniks.stp.wedoit.accord.client.controller.subcontroller.optionsSubCon
 import de.uniks.stp.wedoit.accord.client.Editor;
 import de.uniks.stp.wedoit.accord.client.controller.Controller;
 import de.uniks.stp.wedoit.accord.client.model.Options;
+import de.uniks.stp.wedoit.accord.client.model.User;
 import de.uniks.stp.wedoit.accord.client.richtext.RichTextArea;
 import de.uniks.stp.wedoit.accord.client.util.JsonUtil;
 import javafx.scene.Parent;
@@ -18,7 +19,7 @@ public class DescriptionController implements Controller {
     private VBox vBoxDescription;
     private RichTextArea richTextArea = new RichTextArea();
 
-    public DescriptionController(Parent view, Options model, Editor editor){
+    public DescriptionController(Parent view, Options model, Editor editor) {
         this.view = view;
         this.options = model;
         this.editor = editor;
@@ -26,18 +27,21 @@ public class DescriptionController implements Controller {
 
     @Override
     public void init() {
-        System.out.println(options.getAccordClient().getLocalUser().getUserKey());
-        System.out.println(options.getAccordClient().getLocalUser().getId());
         this.vBoxDescription = (VBox) view.lookup("#vBoxDescription");
+        richTextArea.setId("rTArea");
         vBoxDescription.setMaxWidth(500);
-
+        for (User user : editor.getLocalUser().getUsers()) {
+            if (user.getName().equals(editor.getLocalUser().getName()) && user.getDescription() != null) {
+                editor.getLocalUser().setDescription(user.getDescription());
+                richTextArea.setText(user.getDescription());
+            }
+        }
         vBoxDescription.getChildren().add(richTextArea);
 
     }
 
     @Override
     public void stop() {
-        System.out.println("Called stop");
         if (richTextArea.getText() != null) {
             options.getAccordClient().getLocalUser().setDescription(JsonUtil.buildDescription(CUSTOM_KEY, richTextArea.getText()));
         }
